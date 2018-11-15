438ff2007cf1c990867b7f492aac61c5c89c699c
kieran waugh <kieran.waugh98@gmail.com>
2018-11-15 00:20:08 +0000

    Worked on layout of bank popup window

--- app/src/main/java/com/kieranwaugh/coinz/coinz/BankWindow.java
+++ app/src/main/java/com/kieranwaugh/coinz/coinz/BankWindow.java
@@ -1,8 +1,12 @@
 package com.kieranwaugh.coinz.coinz;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 
 public class BankWindow extends AppCompatActivity {
 
@@ -14,7 +18,11 @@ public class BankWindow extends AppCompatActivity {
         getWindowManager().getDefaultDisplay().getMetrics(dm);
         int width = dm.widthPixels;
         int height = dm.heightPixels;
-        getWindow().setLayout((int)(width * .8), (int)(height * .8));
+        getWindow().setLayout((int)(width * .8), (int)(height * .6));
+
+
+
+
 
     }
 }
